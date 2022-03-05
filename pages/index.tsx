0efcfeeb430d6f0aee24b27894e8d8ede6c9b932
Shubham Patil <shubhampatilsd@gmail.com>
2022-03-05 10:38:40 -0800

    socket.io client stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
 import type { NextPage } from "next";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:4000");
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Bruh moment", socket.id);
+    }); // x8WIv7-mJelg7on_ALbx});
+  }, []);
+
   return <div>GeoChattr</div>;
 };
 
