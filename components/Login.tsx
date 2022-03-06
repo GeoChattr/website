7c54e88661608a41c13a0e9d2e1d21d8ccb42a02
maggie-j-liu <maggie.j.liu@gmail.com>
2022-03-05 16:05:12 -0800

    add styles

--- components/Login.tsx
+++ components/Login.tsx
@@ -2,15 +2,20 @@ const Login = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center">
       <div className="rounded-lg bg-gray-900 px-8 py-6">
-        <h1 className="text-2xl font-semibold">Welcome to GeoChattr</h1>
+        <h1 className="text-2xl font-semibold">
+          Welcome to{" "}
+          <span className="bg-gradient-to-r from-blue-400 to-purple-400 bg-clip-text font-bold text-transparent">
+            GeoChattr
+          </span>
+        </h1>
         <p className="text-gray-300">
           Chat with people in your area through drawings.
         </p>
         <div className="mt-4 flex w-full flex-col items-stretch gap-y-4">
-          <button className="rounded-md bg-gray-800 py-2 px-2 font-semibold">
+          <button className="rounded-md bg-gray-800 py-2 px-2 font-semibold hover:bg-gray-700">
             Log in with Google
           </button>
-          <button className="rounded-md bg-gray-800 py-2 px-2 font-semibold">
+          <button className="rounded-md bg-gray-800 py-2 px-2 font-semibold hover:bg-gray-700">
             Log in with GitHub
           </button>
         </div>
